4c1a7f2d19adc68c57e27a775b9cdaea3c1beaec
kold <alswjd4047@naver.com>
2021-11-27 21:50:11 +0900

    [Week1] 1주차 코드랩 완성

--- week 1-Jetpack Compose basics/KoldComposeWeek1/app/src/main/java/com/kold/koldcomposeweek1/MainActivity.kt
+++ week 1-Jetpack Compose basics/KoldComposeWeek1/app/src/main/java/com/kold/koldcomposeweek1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,37 +39,64 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(                             // 용수철 효과
-            dampingRatio = Spring.DampingRatioMediumBouncy, // 진동감폭(?)
-            stiffness = Spring.StiffnessLow                 // 뻣뻣함(?)
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))     // 값을 강제함. (coerce)
-            ) {
-                Text(text = "Hello, ")
+       CardContent(name = name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    val expanded = rememberSaveable { mutableStateOf(false) }
+//    val extraPadding by animateDpAsState(
+//        targetValue = if (expanded.value) 48.dp else 0.dp,
+//        animationSpec = spring(                             // 용수철 효과
+//            dampingRatio = Spring.DampingRatioMediumBouncy, // 진동감폭(?)
+//            stiffness = Spring.StiffnessLow                 // 뻣뻣함(?)
+//        )
+//    )
+
+    Row(
+        modifier = Modifier
+            .padding(24.dp)
+            .animateContentSize(
+                animationSpec = spring(                             // 용수철 효과
+                    dampingRatio = Spring.DampingRatioMediumBouncy, // 진동감폭(?)
+                    stiffness = Spring.StiffnessLow                 // 뻣뻣함(?)
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+//                .padding(bottom = extraPadding.coerceAtLeast(0.dp))     // 값을 강제함. (coerce)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+            )
+
+            if (expanded.value) {
                 Text(
-                    text = name,
-                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                    text = ("Composem ipsum color sit lazy, pading theme elit, sed do bouncy.").repeat(4)
                 )
             }
+        }
 
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "숨기기" else "펼치기")
-            }
+        IconButton(
+            onClick = { expanded.value = !expanded.value },
+        ) {
+            Icon(
+                imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded.value) {
+                    stringResource(id = R.string.show_less)
+                } else {
+                    stringResource(id = R.string.show_more)
+                }
+            )
         }
     }
 }
